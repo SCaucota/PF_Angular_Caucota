f83d5509df086dba3fe9f9a6de9d2f4bf015aea5
Sofia Caucota <masocaucota@gmail.com>
2024-07-12 23:02:26 -0300

    function-add-alumno

--- src/app/components/abm-alumnos/abm-alumnos.component.ts
+++ src/app/components/abm-alumnos/abm-alumnos.component.ts
@@ -1,7 +1,13 @@
-import { Component, Input, EventEmitter } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 import { AlumnosDialogComponent } from '../alumnos-dialog/alumnos-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { Alumno } from '../../models/alumno';
+
+
+/* const ELEMENT_DATA: Alumno[] = [
+  { id: '1', nombre: 'Juan', apellido: 'Pérez' },
+  { id: '2', nombre: 'María', apellido: 'Gómez' },
+]; */
 
 @Component({
   selector: 'app-abm-alumnos',
@@ -10,8 +16,11 @@ import { Alumno } from '../../models/alumno';
 })
 export class AbmAlumnosComponent {
 
-  @Input() alumnos: Alumno[] = [];
- 
+  @Input() listaAlumnos: Alumno[] = []
+
+  @Output() arrayAlumnos = new EventEmitter<Alumno[]>();
+
+
   constructor(private matDialog: MatDialog) {}
 
   openDialog(): void {
@@ -23,7 +32,17 @@ export class AbmAlumnosComponent {
   }
 
   ondSubmitAlumno(alumno: Alumno): void {
-    this.alumnos.push(alumno)
-    console.log(this.alumnos)
+    const maxId = Math.max(...this.listaAlumnos.map(a => +a.id))
+    const newId = (maxId + 1).toString();
+
+    const {id, ...rest}: Alumno = alumno;
+    const newAlumno: Alumno = {id: newId, ...rest}
+
+    this.listaAlumnos = [...this.listaAlumnos, newAlumno]
+    this.alumnosUpdate();
+  }
+
+  alumnosUpdate(): void {
+    this.arrayAlumnos.emit(this.listaAlumnos);
   }
 }
--- src/app/components/lista-alumnos/lista-alumnos.component.ts
+++ src/app/components/lista-alumnos/lista-alumnos.component.ts
@@ -1,13 +1,11 @@
-import { AfterViewInit, Component, TemplateRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, TemplateRef, ViewChild } from '@angular/core';
 import { Alumno } from '../../models/alumno';
 import { AbmAlumnosComponent } from '../abm-alumnos/abm-alumnos.component';
-
 
 const ELEMENT_DATA: Alumno[] = [
   { id: '1', nombre: 'Juan', apellido: 'Pérez' },
   { id: '2', nombre: 'María', apellido: 'Gómez' },
 ];
-
 
 @Component({
   selector: 'app-lista-alumnos',
@@ -15,17 +13,24 @@ const ELEMENT_DATA: Alumno[] = [
   styleUrl: './lista-alumnos.component.scss'
 })
 export class ListaAlumnosComponent implements AfterViewInit{
-
   @ViewChild(AbmAlumnosComponent) abmAlumnosComponent?: AbmAlumnosComponent;
 
   displayedColumns: string[] = ['id', 'nombre', 'apellido', 'actions'];
+
   dataSource = ELEMENT_DATA;
 
   ngAfterViewInit(): void {
     console.log('AbmAlumnosComponente: ', this.abmAlumnosComponent);
-    if(this.abmAlumnosComponent) {
-      this.abmAlumnosComponent.alumnos = this.dataSource;
+    if (this.abmAlumnosComponent) {
+      this.abmAlumnosComponent.arrayAlumnos.subscribe((updatedAlumnos: Alumno[]) => {
+        this.updateList(updatedAlumnos);
+      });
     }
+  }
+
+  updateList(array: Alumno[]) {
+    this.dataSource = array
+    console.log(this.dataSource)
   }
 
 }
